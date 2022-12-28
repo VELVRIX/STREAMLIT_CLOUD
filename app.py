ce825cc6a37cd1df224fe00d4d0ba8950abc358f
Vivek Kumar <86217421+VELVRIX@users.noreply.github.com>
2022-12-28 21:34:55 +0530

    added some commit

--- app.py
+++ app.py
@@ -42,37 +42,8 @@ if user_input and button :
 
 
 '''
-import streamlit as st
-import numpy as np
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
-import torch
-import numpy as np
+* This model is fine tunned version of RoBERTa-base & has a MCC score of 0.86.
 
-@st.cache(allow_output_mutation=True)
-def get_model():
-    model_x = AutoModelForSequenceClassification.from_pretrained("velvrix/truefoundary_sentimental_RoBERTa")
-    tokenizer_x = AutoTokenizer.from_pretrained("velvrix/truefoundary_sentimental_RoBERTa")
-    return tokenizer_x,model_x
-
-
-tokenizer,model = get_model()
-
-user_input = st.text_area('Enter Text to Analyze')
-button = st.button("Analyze")
-
-d = {
-    
-  1:'Postive Sentiment',
-  0:'Negative Sentiment'
-}
-
-if user_input and button :
-    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
-    # test_sample
-    output = model(**test_sample)
-    # st.write("Logits: ",output.logits)
-    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
-    st.write("Prediction: ",d[y_pred[0]])
-
+* model can be accessed from Hugging Face : https://huggingface.co/velvrix/truefoundary_sentimental_RoBERTa
 '''
 
